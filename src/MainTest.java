b8cd7581585c8cb9826042d62f8758f72de98dd5
DanSonnyk <danielsimi@hotmail.com>
2024-06-13 19:01:01 -0300

    [Feature] Steps - parse initial rows to board                 - parse initialPieces to piece in board                 - Generate all possible positions for a piece in the board [Challenge]

--- src/MainTest.java
+++ src/MainTest.java
@@ -12,6 +12,16 @@ class MainTest {
 
     @Test
     void processCoordinatesScenario01() {
+        main.processCoordinates("01");
+    }
+
+    @Test
+    void processCoordinatesScenario02() {
+        main.processCoordinates("02");
+    }
+
+    @Test
+    void processCoordinatesScenario03() {
         main.processCoordinates("03");
     }
 
